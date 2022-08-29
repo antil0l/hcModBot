7a99c582c68cd4bf2648d90081cb4f3267fe94ad
anti_lol <87977405+antil0l@users.noreply.github.com>
2022-08-29 22:08:57 +0430

    comment

--- cmd.py
+++ cmd.py
@@ -3,7 +3,7 @@ import core
 whiteTrips = []
 blackTrips = []
 # the calls for other commands will happen from here
-
+# every command will start a thread and end it
 
 def handler(command):
     pass
